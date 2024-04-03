a70ddf42854badcc4abc6d535a8a3357c64a6cb1
Kibugi1 <njugunabrian843@gmail.com>
2024-04-03 09:44:47 +0000

    Task 2: Adding CSS & Images

---
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,23 @@
+import $ from 'jquery';
+import _ from 'lodash';
+
+$(document).ready(function() {
+    // Add elements to the DOM
+    $('body').append('<p>Holberton Dashboard</p>');
+    $('body').append('<p>Dashboard data for the students</p>');
+    $('body').append('<button id="start-btn">Click here to get started</button>');
+    $('body').append('<p id="count"></p>');
+    $('body').append('<p>Copyright - Holberton School</p>');
+
+    const updateCounter = () => {
+      let times = $('#count').html() || 0;
+      $('button').on('click', () => {
+        times++;
+        $('#count').html(`${times} clicks on the button`);
+  });
+};
+
+_.debounce(updateCounter, 500);
+updateCounter();
+    });
+});
