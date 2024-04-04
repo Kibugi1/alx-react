602bfee41904b08a4433cb0d0ce6072945de1126
Kibugi1 <njugunabrian843@gmail.com>
2024-04-04 18:41:33 +0300

    Changes in the App.js and App.css files

--- 0x01-react_intro/dashboard/src/App.js
+++ 0x01-react_intro/dashboard/src/App.js
@@ -1,21 +1,21 @@
-import holberton_logo from './holberton_logo.svg';
+import holb_logo from './holb_logo.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={holberton_logo} className="App-logo" alt="logo" />
+        <img src={holb_logo} className="App-logo" alt="logo" />
         <p>
           School dashboard
         </p> 
       </header>
-      <hr />
+      <hr className="App-hrtop" />
       <div className="App-body">
         <p>Login to access the full dashboard</p>
       </div>
-      <hr />
       <footer className="App-footer">
+      <hr className="App-hrb" />
         <p>Copyright 2020 - Holberton School</p>
       </footer>
     </div>
