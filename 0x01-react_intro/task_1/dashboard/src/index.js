0184974cd83015231f0758e6d7dad0e8b449627e
Kibugi1 <njugunabrian843@gmail.com>
2024-04-05 19:49:21 +0300

    Changes in index.js

--- 0x01-react_intro/task_1/dashboard/src/index.js
+++ 0x01-react_intro/task_1/dashboard/src/index.js
@@ -1,9 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import Notifications from './Notifications';
-
-import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import Notifications from './Notifications';
