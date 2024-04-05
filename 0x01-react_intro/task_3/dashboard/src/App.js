e08c5bdada9e38c3ac75143fe2c77536b7f15705
Kibugi1 <njugunabrian843@gmail.com>
2024-04-05 20:41:32 +0300

    Task_3

--- 0x01-react_intro/task_3/dashboard/src/App.js
+++ 0x01-react_intro/task_3/dashboard/src/App.js
@@ -13,7 +13,12 @@ function App() {
       </header>
       <hr className="App-hrtop" />
       <div className="App-body">
-        <p>Login to access the full dashboard</p>
+            <p>Login to access the full dashboard</p>
+        <label htmlFor="email">Email :</label>
+        <input name="email" type="email" id="email"></input>
+        <label htmlFor="password">Password :</label>
+        <input name="password" type="password" id="password"></input>
+        <button>OK</button>
       </div>
       <footer className="App-footer">
       <hr className="App-hrb" />
