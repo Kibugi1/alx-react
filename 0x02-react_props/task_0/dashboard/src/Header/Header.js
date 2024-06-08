5a5047c00290447b16191ae0f69e33f7a6eafe03
Kibugi1 <njugunabrian843@gmail.com>
2024-06-08 15:05:22 +0300

    Task_1 for react_props

---
+++ 0x02-react_props/task_0/dashboard/src/Header/Header.js
@@ -0,0 +1,16 @@
+	import { getFullYear, getFooterCopy } from './utils';
+import logo from './holberton-logo.jpg';
+import './Header.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
